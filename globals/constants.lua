9c87cf02440d68ed960d98a9dc05c31e69c03c33
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-29 15:09:38 -0300

    feat: creates constants file

---
+++ globals/constants.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+M.WINDOW_WIDTH = 1280
+M.WINDOW_HEIGHT = 720
+
+M.VIRTUAL_WIDTH = 512
+M.VIRTUAL_HEIGHT = 288
+
+M.BACKGROUND = love.graphics.newImage("assets/background.png")
+M.GROUND = love.graphics.newImage("assets/ground.png")
+
+return M
