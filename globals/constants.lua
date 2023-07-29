29843f2ce320e906cd38811815bee5657f4f5d2f
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-29 15:29:11 -0300

    feat: adds scroll effects to background

--- globals/constants.lua
+++ globals/constants.lua
@@ -7,6 +7,11 @@ M.VIRTUAL_WIDTH = 512
 M.VIRTUAL_HEIGHT = 288
 
 M.BACKGROUND = love.graphics.newImage("assets/background.png")
+M.BACKGROUND_SCROLL_SPEED = 30
+-- the point on the X axis in which we will render another copy of the image
+-- to give an effect of an infinite scrolling background
+M.BACKGROUND_LOOPING_POINT = 413
+
 M.GROUND = love.graphics.newImage("assets/ground.png")
 
 return M
