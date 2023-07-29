296fe7f6f2ea2f7af5d320ddc8649bf29e90f7b3
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-29 15:32:05 -0300

    refact: small adjustments

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local push = require("lib.push")
 local c = require("globals.constants")
+local v = require("globals.variables")
 
 -- executed at the beginning of the execution
 function love.load()
@@ -23,9 +24,15 @@ function love.keypressed(key)
 	end
 end
 
+function love.update(dt)
+	-- dt is here to make the game framerate independent, in other words, the game will run at the same speed independently of the framerate
+	v.backgroundScroll = (v.backgroundScroll + c.BACKGROUND_SCROLL_SPEED * dt) % c.BACKGROUND_LOOPING_POINT
+	v.groundScroll = (v.groundScroll + c.GROUND_SCROLL_SPEED * dt) % c.GROUND_LOOPING_POINT
+end
+
 function love.draw()
 	push:start()
-	love.graphics.draw(c.BACKGROUND, 0, 0)
 	love.graphics.draw(c.BACKGROUND, -v.backgroundScroll, 0)
 	love.graphics.draw(c.GROUND, -v.groundScroll, c.VIRTUAL_HEIGHT - 16)
+	push:finish()
 end
