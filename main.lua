1eb3b2f98a39417a068bf1632a94e94ac0a12331
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-30 16:35:43 -0300

    feat: updates on main.lua

--- main.lua
+++ main.lua
@@ -28,11 +28,16 @@ function love.update(dt)
 	-- dt is here to make the game framerate independent, in other words, the game will run at the same speed independently of the framerate
 	v.backgroundScroll = (v.backgroundScroll + c.BACKGROUND_SCROLL_SPEED * dt) % c.BACKGROUND_LOOPING_POINT
 	v.groundScroll = (v.groundScroll + c.GROUND_SCROLL_SPEED * dt) % c.GROUND_LOOPING_POINT
+
+	bird:update(dt)
 end
 
 function love.draw()
 	push:start()
 	love.graphics.draw(c.BACKGROUND, -v.backgroundScroll, 0)
 	love.graphics.draw(c.GROUND, -v.groundScroll, c.VIRTUAL_HEIGHT - 16)
+
+	bird:render()
+
 	push:finish()
 end
