d1f23c62ff4ca9b5d97162d56dc52990a4eab142
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-08 10:11:57 -0300

    feat: creates class to deal with the state when is the menu screen

---
+++ state/entities/TitleScreenState.lua
@@ -0,0 +1,21 @@
+local BaseState = require("state.entities.BaseState")
+local Class = require("lib.class")
+local c = require("globals.constants")
+
+local TitleScreenState = Class({ __includes = BaseState })
+
+function TitleScreenState:update()
+	if love.keyboard.wasPressed("enter") or love.keyboard.wasPressed("return") then
+		StateHandler:change("play")
+	end
+end
+
+function TitleScreenState:render()
+	love.graphics.setFont(c.FLAPPY_FONT)
+	love.graphics.printf("Flappy Bird", 0, 64, c.VIRTUAL_WIDTH, "center")
+
+	love.graphics.setFont(c.MEDIUM_FONT)
+	love.graphics.printf("Press Enter", 0, 100, c.VIRTUAL_WIDTH, "center")
+end
+
+return TitleScreenState
